dae5aa1786568b208271e4cdd052e8635fda1094
hamel <mintimel@gmail.com>
2023-10-04 21:13:10 +0900

    start day + end day 조회 준비..2

--- src/main/java/com/springboot/stock/data/repository/StockRepository.java
+++ src/main/java/com/springboot/stock/data/repository/StockRepository.java
@@ -3,9 +3,11 @@ package com.springboot.stock.data.repository;
 import com.springboot.stock.data.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    /*List<Stock> findAllByStIdAndAndStBefore*/
 }
